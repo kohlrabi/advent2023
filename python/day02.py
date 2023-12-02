87dd35f1c1739b5f73150938ccc00fd83c04018f
Jan Christoph Terasa <christoph@kohlio.de>
2023-12-02 10:48:26 +0100

    Solved day02 part 2

--- python/day02.py
+++ python/day02.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import fileinput
+import functools
 import itertools
 import re
+from collections import defaultdict
 from collections.abc import Iterable
 
 re_game = re.compile(r"Game (\d+):")
@@ -47,7 +49,26 @@ def part1(input: Iterable[str]) -> int:
 
 
 def part2(input: Iterable[str]) -> int:
-    return 0
+    s = 0
+
+    for line in input:
+        match = re_game.match(line)
+
+        min_cubes: dict[str, int] = defaultdict(int)
+
+        games = line.split(":")[1].split(";")
+
+        for game in games:
+            cubes = game.split(",")
+            for cube in cubes:
+                match = re_cube.match(cube)
+                if match:
+                    num, color = match.groups()
+                    min_cubes[color] = max(min_cubes[color], int(num))
+
+        s += functools.reduce(lambda x, y: x * y, min_cubes.values())
+
+    return s
 
 
 def main() -> None:
